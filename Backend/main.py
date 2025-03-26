c30094d7c23fe25eda825844a8a8295e6e41a3ba
VIGHNESH M S <princevighnesh71@gmail.com>
2025-03-26 21:09:00 +0530

    recieving and transcribing

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
+# from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import aiofiles
+import whisper
+import os
 
+model = whisper.load_model("base")
 app = FastAPI()
 
 # CORS Middleware to allow frontend requests
@@ -13,10 +17,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Data model for input text
-class TextInput(BaseModel):
-    text: str
 
-@app.post("/submit")
-async def receive_text(data: TextInput):
-    return {"message": f"You wrote: {data.text}"}
+# New endpoint for audio upload
+@app.post("/submit-audio")
+async def receive_audio(audio: UploadFile = File(...)):
+    # Save the file temporarily (optional)
+    out_file_path = f"temp_audio_{audio.filename}"
+    
+    async with aiofiles.open(out_file_path, 'wb') as out_file:
+        content = await audio.read()  # Read the file content
+        await out_file.write(content)  # Write to disk
+    
+    result = model.transcribe(out_file_path )
+    return {
+        "message": result["text"],
+        "filename": audio.filename,
+        "content_type": audio.content_type,
+        "size": f"{len(content) / 1024:.2f} KB"
+    }
+
+
