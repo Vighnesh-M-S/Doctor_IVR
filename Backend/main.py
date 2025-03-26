a300e8829f0cb530afeded8f0c6fa3731022c225
VIGHNESH M S <princevighnesh71@gmail.com>
2025-03-26 22:11:07 +0530

    getting gemini response in text

--- Backend/main.py
+++ Backend/main.py
@@ -3,9 +3,16 @@ from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import aiofiles
 import whisper
+from google import genai
+from dotenv import load_dotenv
 import os
 
+
+
 model = whisper.load_model("base")
+load_dotenv() 
+api_key2 = os.getenv("API_KEY")
+client = genai.Client(api_key=api_key2)
 app = FastAPI()
 
 # CORS Middleware to allow frontend requests
@@ -18,6 +25,7 @@ app.add_middleware(
 )
 
 
+
 # New endpoint for audio upload
 @app.post("/submit-audio")
 async def receive_audio(audio: UploadFile = File(...)):
@@ -28,12 +36,29 @@ async def receive_audio(audio: UploadFile = File(...)):
         content = await audio.read()  # Read the file content
         await out_file.write(content)  # Write to disk
     
-    result = model.transcribe(out_file_path )
+    result = model.transcribe(out_file_path)
+    transcribed_text = result["text"]
+
+     # Generate response using Gemini API
+    # gemini_model = genai.GenerativeModel("gemini-pro")
+    # response = gemini_model.generate_content(f"""
+    # A patient reports these ENT symptoms:\n{transcribed_text}
+    # Based on these symptoms, ask the patient a relevant follow-up question to narrow down possible causes.
+    # """)
+    response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=f"""
+    A patient reports these ENT symptoms:\n{transcribed_text}
+    Based on these symptoms, ask the patient a relevant follow-up question to narrow down possible causes.
+    """,
+)
+
+    # Cleanup temp file
+    # os.remove(out_file_path)
+
     return {
-        "message": result["text"],
-        "filename": audio.filename,
-        "content_type": audio.content_type,
-        "size": f"{len(content) / 1024:.2f} KB"
+        "message": transcribed_text + response.text,
+        "gemini_reply": response.text if response.text else "No response generated",
     }
 
 
