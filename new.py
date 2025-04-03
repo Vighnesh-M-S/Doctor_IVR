0657a6b6fdafaf3fd9afc55b98bc3ed05f2f74d6
VIGHNESH M S <princevighnesh71@gmail.com>
2025-04-04 00:35:50 +0530

    changes

--- new.py
+++ new.py
@@ -24,7 +24,7 @@ def download_twilio_audio(url, account_sid, auth_token, output_filename="audio2.
         print(f"An unexpected error occurred: {e}")
 
 # Example usage:
-twilio_url = "https://api.twilio.com/2010-04-01/Accounts/ACd732e6a1635e6225b8d0b6dd69cdc4e5/Recordings/RE52dfa587d54aed0cde894fef81d8de0b"
+twilio_url = ""
 account_sid = os.getenv("TWILIO_SID")
 auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 
